4d871cf9384470a5004ac13980ed1fbe2e15329e
cameron <cameron.kueneman@gmail.com>
2022-07-18 15:51:07 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Threading;
+using rps.models;
+
+bool playing = true;
+Game RPS = new Game();
+
+int compScore = 0;
+int playerScore = 0;
+
+string intro = @"                                
+    //   ) )  //   ) ) //   ) ) 
+   //___/ /  //___/ / ((        
+  / ___ (   / ____ /    \\      
+ //   | |  //             ) )   
+//    | | //       ((___ / /    ";
+string intro2 = "N to stop playing, or any R P or S to play.";
+
+while(playing)
+{
+  Console.Clear();
+  Console.WriteLine(intro);
+  for (int i = 0; i < intro2.Length; i++)
+  {
+    Console.Write(intro2[i]);
+    Thread.Sleep(20);
+  }
+  Console.WriteLine("");
+  Console.WriteLine($"Player Score: {playerScore} - - - - - Enemy Kill Count {compScore}");
+  string keepPlaying = Console.ReadLine();
+
+
+  if (keepPlaying == "n") {
+    Console.WriteLine("Scrub");
+    playing = false;
+  }
+  if (keepPlaying == "r" || keepPlaying ==  "p" || keepPlaying ==  "s") {
+
+    string result = RPS.PlayRound(keepPlaying);
+    Console.WriteLine(result);
+
+    if (result == "Win") {
+      playerScore += 1;
+      superMario();
+    }
+    
+    if (result =="Loser") {
+      compScore += 1;
+    }
+    Thread.Sleep(2000);
+  }
+}
+
+void superMario() {
+  Console.Beep(659, 125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(523, 125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(375); Console.Beep(392, 125); Thread.Sleep(375); Console.Beep(523, 125); Thread.Sleep(250); Console.Beep(392, 125); Thread.Sleep(250); Console.Beep(330, 125); Thread.Sleep(250); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(466, 125); Thread.Sleep(42); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(392, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(880, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(587, 125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(250); Console.Beep(392, 125); Thread.Sleep(250); Console.Beep(330, 125); Thread.Sleep(250); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(466, 125); Thread.Sleep(42); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(392, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(880, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(587, 125); Console.Beep(494, 125); Thread.Sleep(375); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(698, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(698, 125); Thread.Sleep(625); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(622, 125); Thread.Sleep(250); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(523, 125); Thread.Sleep(1125); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(698, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(698, 125); Thread.Sleep(625); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(622, 125); Thread.Sleep(250); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(523, 125);
+}
---
+++ models/Game.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rps.models
+{
+    public class Game
+    {
+        Random rnd = new Random(); 
+        List<string> choices = new List<string>() {
+            "r","p","s"
+        };
+        public string PlayRound(string choice) {
+            string computerChoice = ComputerAction();
+            
+            if (choice == "r" && computerChoice == "s") { return "Win";};
+            if (choice == "r" && computerChoice == "p") { return "Loser";};
+            if (choice == "r" && computerChoice == "r") { return "Tie";};
+            if (choice == "s" && computerChoice == "p") { return "Win";};
+            if (choice == "s" && computerChoice == "r") { return "Loser";};
+            if (choice == "s" && computerChoice == "s") { return "Tie";};
+            if (choice == "p" && computerChoice == "r") { return "Win";};
+            if (choice == "p" && computerChoice == "s") { return "Loser";};
+            if (choice == "p" && computerChoice == "p") { return "Tie";};
+            return "Something went wrong";
+        }
+
+        private string ComputerAction() {
+            int num = rnd.Next(0,2);
+            return choices[num];
+        }
+    }
+}
